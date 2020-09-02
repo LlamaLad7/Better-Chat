0ae27f1fb238abdd8108233ad92d3578b8b28fbc
lego3708 <l3gomindstorms@gmail.com>
2020-09-02 20:49:52 +0100

    Bump version

--- src/main/java/com/llamalad7/betterchat/BetterChat.java
+++ src/main/java/com/llamalad7/betterchat/BetterChat.java
@@ -34,7 +34,7 @@ public class BetterChat
 {
     public static final String MODID = "betterchat";
     public static final String NAME = "Better Chat";
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.4";
     private static ChatSettings settings;
 
     @EventHandler
